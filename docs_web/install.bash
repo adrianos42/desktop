c0dbfbe485c722c4a03ab6bc74f7e8dba7fc0b60
Adriano <adriano.souza113@gmail.com>
2021-08-20 23:52:54 -0300

    Fixed progress indicator animation.

---
+++ docs_web/install.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# idl client --path=plugins/collections
+flutter build linux --verbose --release
+rm -rf ~/opt/desktop_docs
+mkdir -p ~/opt/desktop_docs
+cp -r build/linux/x64/release/bundle/* ~/opt/desktop_docs/
+cp assets/icon_transparent.svg ~/opt/desktop_docs/icon.svg
+echo "#!/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Terminal=false
+Type=Application
+Name=Desktop Docs" >> ~/.local/share/applications/desktop_docs.desktop
+echo "" >> ~/.local/share/applications/desktop_docs.desktop
+echo "Exec=$HOME/opt/desktop_docs/docs_web" >> ~/.local/share/applications/desktop_docs.desktop
+echo "Icon=$HOME/opt/desktop_docs/icon.svg" >> ~/.local/share/applications/desktop_docs.desktop
